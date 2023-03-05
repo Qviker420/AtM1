4d9505d9741953507e315a542922f75c22022bf4
chali <chaligavaduta3@gmail.com>
2023-03-05 09:35:30 -0800

    Adding Comments

--- app/src/main/java/com/example/agetominutes/MainActivity.kt
+++ app/src/main/java/com/example/agetominutes/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //creating variables
         val datePickBtn:Button = findViewById(R.id.datePciker)
         enteredDate = findViewById(R.id.yourDate)
         val result: TextView = findViewById(R.id.ageToMins)
